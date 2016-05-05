27d604ee3b215b85f7a98750dece885abd2e213d
benkoo <koo0905@gmail.com>
2016-05-05 14:12:56 +0800

    Added a font directory and a font file.

--- Tetromino.py
+++ Tetromino.py
@@ -160,8 +160,8 @@ def main():
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
-    BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
+    BASICFONT = pygame.font.Font('assets/font/freesansbold.ttf', 18)
+    BIGFONT = pygame.font.Font('assets/font/freesansbold.ttf', 100)
     pygame.display.set_caption('Tetromino')
 
     showTextScreen('Tetromino')
