944ccf2aff022bff0e5680ae33a6ee5ebab75010
benkoo <koo0905@gmail.com>
2016-05-05 17:59:48 +0800

    Added two more examples

---
+++ ExtendedImageTest.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+if pygame.image.get_extended() == 0:
+    print("Extended Image Package NOT available")
+    SystemError
+    sys.exit()
+else:
+    print("Finally have Extended Image Package, now")
+
+SystemExit
